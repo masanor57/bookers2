74dab6653f6aa89b666281d594b31e89e26e13a0
masanor57 <miyajim2020@gmail.com>
2021-07-02 14:26:24 +0000

    ajast5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_many :books, dependent: :destroy
   attachment :profile_image
- 
+
   validates :name, presence: true, uniqueness: true, on: :create
   validates :name, presence: true, length: { minimum: 2, maximum: 20}
   validates :introduction, presence: false, length: { maximum: 50 }
